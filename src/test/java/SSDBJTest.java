aadff5f257129be1ac58caf13693d997db40a492
jobell.jiang <jobell@qq.com>
2014-12-31 16:12:15 +0800

    连接池测试程序添加

--- src/test/java/SSDBJTest.java
+++ src/test/java/SSDBJTest.java
@@ -1,14 +1,9 @@
 import java.util.ArrayList;
 import java.util.Properties;
 
-import org.apache.commons.pool2.impl.AbandonedConfig;
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
-
-import com.lovver.ssdbj.core.BaseConnection;
+import com.lovver.ssdbj.core.SSDBDriver;
+import com.lovver.ssdbj.core.impl.SSDBConnection;
 import com.lovver.ssdbj.core.impl.SSDBResultSet;
-import com.lovver.ssdbj.pool.ConnectionPool;
-import com.lovver.ssdbj.pool.PoolConnection;
-import com.lovver.ssdbj.pool.PooledConnectionFactory;
 
 
 
@@ -20,48 +15,31 @@ public class SSDBJTest {
 	 */
 	@SuppressWarnings({ "serial", "rawtypes", "unchecked" })
 	public static void main(String[] args) throws Exception {
-//		SSDBDriver dd= new SSDBDriver();
-//		Properties info = new Properties();
-//		info.setProperty("user", "test");
-//		info.setProperty("password", "ddd");
-//		info.setProperty("loginTimeout", "300");
-//		info.setProperty("tcpKeepAlive", "true");
-//		info.setProperty("protocolName", "ssdb");
-//		info.setProperty("protocolVersion", "ddd");
-//		
-//		info.setProperty("SSDB_HOST", "192.168.0.226");
-//		info.setProperty("SSDB_PORT", "8888");
-//		SSDBConnection conn= dd.connect(info);
-//		ArrayList<byte[]> setparams=new ArrayList<byte[]>(){
-//			{
-//				add("joliny".getBytes());
-//				add("kkk".getBytes());
-//				add("是的发生地发生1231sdfsfg23".getBytes());
-//			}
-//		};
-//		conn.execute("hset",setparams);
-//		
-//		ArrayList params=new ArrayList();
-//		params.add("joliny".getBytes());
-//		params.add("kkk".getBytes());
-//		SSDBResultSet rs=conn.execute("hget",params );
-//		System.out.println(new String(rs.getResult()));
-		
-		
+		SSDBDriver dd= new SSDBDriver();
 		Properties info = new Properties();
+		info.setProperty("user", "test");
 		info.setProperty("password", "ddd");
 		info.setProperty("loginTimeout", "300");
 		info.setProperty("tcpKeepAlive", "true");
 		info.setProperty("protocolName", "ssdb");
 		info.setProperty("protocolVersion", "ddd");
-		PooledConnectionFactory<BaseConnection> factory = new PooledConnectionFactory("192.168.0.226",8888,null,info);
-		ConnectionPool pools = new ConnectionPool(factory,new GenericObjectPoolConfig(),new AbandonedConfig());
-		PoolConnection conn=(PoolConnection) pools.borrowObject();
+		
+		info.setProperty("SSDB_HOST", "192.168.0.226");
+		info.setProperty("SSDB_PORT", "8888");
+		SSDBConnection conn= dd.connect(info);
+		ArrayList<byte[]> setparams=new ArrayList<byte[]>(){
+			{
+				add("joliny".getBytes());
+				add("kkk".getBytes());
+				add("是的发生地发生1231sdfsfg23".getBytes());
+			}
+		};
+		conn.execute("hset",setparams);
 		
 		ArrayList params=new ArrayList();
 		params.add("joliny".getBytes());
 		params.add("kkk".getBytes());
-		SSDBResultSet rs=(SSDBResultSet) conn.execute("hget",params );
+		SSDBResultSet rs=conn.execute("hget",params );
 		System.out.println(new String(rs.getResult()));
 	}
 
---
+++ src/test/java/SSDBPoolTest.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Properties;
+
+import org.apache.commons.pool2.impl.AbandonedConfig;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+import com.lovver.ssdbj.core.BaseConnection;
+import com.lovver.ssdbj.core.impl.SSDBResultSet;
+import com.lovver.ssdbj.pool.ConnectionPool;
+import com.lovver.ssdbj.pool.PoolConnection;
+import com.lovver.ssdbj.pool.PooledConnectionFactory;
+
+
+public class SSDBPoolTest {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	@SuppressWarnings({  "rawtypes", "unchecked" })
+	public static void main(String[] args) throws Exception {
+		Properties info = new Properties();
+		info.setProperty("password", "ddd");
+		info.setProperty("loginTimeout", "300");
+		info.setProperty("tcpKeepAlive", "true");
+		info.setProperty("protocolName", "ssdb");
+		info.setProperty("protocolVersion", "1.0");
+		PooledConnectionFactory<BaseConnection> factory = new PooledConnectionFactory("192.168.0.226",8888,null,info);
+		ConnectionPool pools = new ConnectionPool(factory,new GenericObjectPoolConfig(),new AbandonedConfig());
+		PoolConnection conn=(PoolConnection) pools.borrowObject();
+		
+		ArrayList params=new ArrayList();
+		params.add("joliny".getBytes());
+		params.add("kkk".getBytes());
+		SSDBResultSet rs=(SSDBResultSet) conn.execute("hget",params );
+		pools.returnObject(conn);
+		System.out.println(new String(rs.getResult()));
+	}
+
+}
